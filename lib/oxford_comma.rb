9aea12968dd0a7af0d512e07fedb91bb96cb17ba
Madeline Celis <madelinebcelis@gmail.com>
2018-07-23 05:33:16 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,5 +3,7 @@ def oxford_comma(array)
      return array[0]
    elsif array.size == 2
      return "#{array[0] and array[1]}"
+   elsif array.size == 3
+     return "#{array[0]}#{array[1]} and #{array[2]}"
    end
 end
