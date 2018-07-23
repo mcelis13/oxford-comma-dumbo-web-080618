12288a02595346ca96e3ad1eccfcb12c67397d1d
Madeline Celis <madelinebcelis@gmail.com>
2018-07-23 05:34:37 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
-   if array.size == 1
+   if array.size === 1
      return array[0]
-   elsif array.size == 2
+   elsif array.size === 2
      return "#{array[0] and array[1]}"
-   elsif array.size == 3
+   elsif array.size === 3
      return "#{array[0]}#{array[1]} and #{array[2]}"
    else
      return array.join(',')
