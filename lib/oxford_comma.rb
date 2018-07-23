051f5d91be81cb5b81b0f9697990f30eb5b09429
Madeline Celis <madelinebcelis@gmail.com>
2018-07-23 05:46:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,6 @@ def oxford_comma(array)
    elsif array.size === 3
      return "#{array[0]}, #{array[1]}, and #{array[2]}"
    else
-     return array.split(' ')
+     return array.slice(0, (array.size - 1))
    end
 end
