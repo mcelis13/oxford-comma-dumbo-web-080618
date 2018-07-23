892c883d21634000036033ec569ff223ff822301
Madeline Celis <madelinebcelis@gmail.com>
2018-07-23 05:49:08 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(array)
      return "#{array[0]}, #{array[1]}, and #{array[2]}"
    else
      newArray = array.slice(0, (array.size - 1))
-     newArray.push('and').push(array[array.size -1])
+     newArray.concat(['and']).push(array[array.size -1])
      return newArray
    end
 end
