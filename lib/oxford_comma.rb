346743d4221dff403b01e9dbd78515678763abbb
Madeline Celis <madelinebcelis@gmail.com>
2018-07-23 05:59:38 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,9 +7,9 @@ def oxford_comma(array)
      return "#{array[0]}, #{array[1]}, and #{array[2]}"
    else
      newArray = array.slice(0, (array.size - 1))
-     newArray.concat(["and #{array[-1]}"])
      string = ''
-     newArray.each{|index| string += "#{index}, "}
+     string = newArray.concat(["and #{array[-1]}"])
+
      return string
    end
 end
