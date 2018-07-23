81e6acf591264d4ea75c38cc43fa7378263d9ad1
Madeline Celis <madelinebcelis@gmail.com>
2018-07-23 06:01:01 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,6 @@ def oxford_comma(array)
      return "#{array[0]}, #{array[1]}, and #{array[2]}"
    else
      newArray = array.slice(0, (array.size - 1)).concat(["and #{array[-1]}"])
-
      return newArray
    end
 end
